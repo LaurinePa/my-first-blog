4940c8902f4c34ba2ed8fe50df0ac13b2a348be3
Laurine <laurine.patzer@web.de>
2021-02-12 09:40:04 +0100

    HTML der Site geändert

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 08:58:11 2021
+
+@author: Laurine
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
